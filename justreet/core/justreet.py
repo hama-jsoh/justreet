fe51fcc16c908f40980ce7e7d2bd98dce1f7df7d
Juseok Oh <111451710+hama-jsoh@users.noreply.github.com>
2022-11-22 07:11:12 +0000

    :sparkles: Implement Justreet abstract class

---
+++ justreet/core/justreet.py
@@ -0,0 +1,13 @@
+from abc import ABCMeta, abstractmethod
+
+
+class Justreet(metaclass=ABCMeta):
+    def TrainSP(self):
+        self.use_resnet.train()
+
+    def TrainOD(self):
+        self.use_yolov5.train()
+
+    @abstractmethod
+    def display():
+        pass

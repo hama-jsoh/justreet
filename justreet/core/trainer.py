718f13039ac9741cb91c7e4832945e42d75b6b10
Juseok Oh <111451710+hama-jsoh@users.noreply.github.com>
2022-11-22 07:13:06 +0000

    :sparkles: Implement trainer

---
+++ justreet/core/trainer.py
@@ -0,0 +1,26 @@
+from justreet import Justreet
+from model import ResNet, YOLOv5
+
+
+class SPTrainer(Justreet):
+    def __init__(self) -> None:
+        self.use_resnet = ResNet()
+
+    def display():
+        print("train...")
+        
+
+class ODTrainer(Justreet):
+    def __init__(self) -> None:
+        self.use_yolov5 = YOLOv5()
+    
+    def display():
+        print("train...")
+
+
+if __name__ == "__main__":
+    sp = SPTrainer()
+    od = ODTrainer()
+
+    sp.TrainSP()
+    od.TrainOD()
